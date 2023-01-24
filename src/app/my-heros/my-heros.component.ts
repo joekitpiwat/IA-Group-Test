0c1556ad62ad854f118616af14723130073e1399
joekitpiwat <joekitpiwat@gmail.com>
2023-01-25 06:36:10 +0700

    init project

---
+++ src/app/my-heros/my-heros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-heros',
+  templateUrl: './my-heros.component.html',
+  styleUrls: ['./my-heros.component.scss']
+})
+export class MyHerosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
